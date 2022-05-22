54b270755aba3c8ec7eb7d25a2e8c0f290188473
Mauricio Sibulski <mauricio.sibulski@outlook.com>
2022-05-22 16:18:14 +1200

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var superheroes = require('superheroes');
+var supervillains = require('supervillains');
+
+var supa1 = superheroes.random();
+var villain1 = supervillains.random();
+console.log(supa1 + " fights " + villain1);
